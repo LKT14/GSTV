44c0f72b47ac1c437c36c6acc716824503d2334b
LKT14 <76061590+LKT14@users.noreply.github.com>
2021-03-01 23:17:05 +0530

    Update js_gif Engine.js

--- js_gif Engine.js
+++ js_gif Engine.js
@@ -1,27 +1,24 @@
 document.querySelector(".js-go").addEventListener('click',function(){
 
     var input = document.querySelector("input").value;
-    s = input;
-    pushToDOM(input);
+    SearchGiphy(input);
   
   });
   
   document.querySelector(".js-userinput").addEventListener('keyup',function(e){
   
-    var ainput = document.querySelector("input").value;
+    var input = document.querySelector("input").value;
   
     // if the key ENTER is pressed...
     if(e.which === 13) {
-      pushToDOM(input);
+      SearchGiphy(input);
     }
   
   });
   
   /* 2. do the data stuff with the API */
-  
-  var url = "https://api.giphy.com/v1/gifs/search?q="+ s;
-             
-
+  function SearchGiphy(input){
+  var url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + input;
   // AJAX Request
   var GiphyAJAXCall = new XMLHttpRequest();
   GiphyAJAXCall.open( 'GET', url );
@@ -31,8 +28,10 @@ document.querySelector(".js-go").addEventListener('click',function(){
   
     var data = e.target.response;
     pushToDOM(data);
+    console.log(data);
   
   });
+  }  
   
   
   
@@ -45,15 +44,19 @@ document.querySelector(".js-go").addEventListener('click',function(){
     var response = JSON.parse(input);
   
     var imageUrls = response.data;
+
+    var container = document.querySelector(".js-container");
+    container.innerHTML = "";
   
     imageUrls.forEach(function(image){
   
       var src = image.images.fixed_height.url;
-      console.log(src);
+      //console.log(src);
   
-      var container = document.querySelector(".js-container");
+      
       container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
-  
+      
+
     });
   
   }
